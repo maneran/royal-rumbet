a7e0e1ecab6a50fb21e1b1b8030483c2d7bebebe
Sagi Salomon <ssalomon@allot.com>
2021-07-15 11:25:42 +0300

    fixed dynamic table generator
    now module can be used by various components .
    added dynamic method to manage the data input for the <td> output

--- royalrumbet_app/src/components/table_gen.js
+++ royalrumbet_app/src/components/table_gen.js
@@ -20,43 +20,55 @@ function TableGen(props) {
     console.log(td_vals)
     const tds = td_vals.map((rowid,idx1)=> {
 
-        let itemAmnt = props.data.rows[Number(rowid)]
-
-        //let std_vals_jsx = itemAmnt.map((colname,idx3) => {
+        let item_values = props.data.rows[Number(rowid)]
+        let listthis = item_values
+        //this will allos dynamic data strucrue 
+        //that can be handled by this generator
+        //cann get {} or [{}]
+        if (Array.isArray(item_values)) {
+            listthis = item_values
+        }
+        else {
+            listthis = [item_values]
+        } 
+        let std_vals_jsx = listthis.map((obj,idx3) => {
             
             let td_vals_jsx = th_vals.map((colname,idx2) => {
                 // need a better error handle
                 // also consider using redirect
                 //also - consider using route in the rh and td it self
-                try {
-                    (() => itemAmnt)()
-                    // (() => props.data.rows[Number(rowid)][colname])()
-                }
-                catch {
+                // try {
+                //     (() => itemAmnt)()
+                //     // (() => props.data.rows[Number(rowid)][colname])()
+                // }
+                // catch {
 
-                    (() => {alert('no such Patch');
-                            history.push('/');
-                            })()
-                }
+                //     (() => {alert('no such Patch');
+                //             history.push('/');
+                //             })()
+                // }
+                console.log(obj[colname]);
 
                 return (
-                    <td key={idx2}>{itemAmnt[colname]}</td>
+                    <td key={idx2}>{obj[colname]}</td>
                 ) 
             })
             /* ADD GET PARAMS FROM LOCATION - IF ACCESS IS FROM URK IT SELF */
             /*will add a change route path to the url to generante the tornament details*/
+            console.log(idx3);
             return (
-                     <tr key={idx1} id={rowid} onClick={() => history.push('/torna' + rowid)} > 
+                     <tr key={idx1+idx3} id={rowid} onClick={() => history.push('/torna' + rowid)} > 
                        {td_vals_jsx}
                      </tr>
             )
         })
-    //     console.log(std_vals_jsx)
-    //     return (
-    //         {std_vals_jsx}
-    //     )
-
-  //  })  
+        console.log(std_vals_jsx)
+        return (
+            <tbody>
+                {std_vals_jsx}
+            </tbody>
+        )
+  })  
 
     const ths = th_vals.map((coln, idx3) => {
         return (
@@ -73,9 +85,7 @@ function TableGen(props) {
                    {ths}
                 </tr>
                 </thead>
-                <tbody>
                      {tds}
-                </tbody>
             </table>
         </Fragment>
     )
